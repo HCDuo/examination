10897a6679eaf07b6bd096cd4f32095f58a6e695
HCDUO <2650591287@qq.com>
2023-07-14 08:43:39 +0800

    Create README.md

--- edu/src/main/java/com/neu/edu/controller/ExamScheduleController.java
+++ edu/src/main/java/com/neu/edu/controller/ExamScheduleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/examschedules")
+@RequestMapping("/schedules")
 public class ExamScheduleController {
 
     @Autowired
